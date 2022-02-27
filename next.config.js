3a2b61f5c70a478606b9b69d2b75a0e99656ff07
Rohitvpatil0810 <rohitvpatil0810@gmail.com>
2022-02-27 12:48:28 +0530

    Final version

--- next.config.js
+++ next.config.js
@@ -3,16 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/https://meme-api.herokuapp.com/gimme/:path*",
-        destination: "https://meme-api.herokuapp.com/gimme/:path*",
-      },
-    ];
-  },
-  images: {
-    domains: ["https://meme-api.herokuapp.com/gimme"],
-  },
-};
+module.exports = nextConfig;
