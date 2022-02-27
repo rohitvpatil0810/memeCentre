459c8a82010582bcca60382bd240fdd97a279f37
Rohitvpatil0810 <rohitvpatil0810@gmail.com>
2022-02-27 12:45:23 +0530

    final version

--- pages/api/get_index_data.js
+++ pages/api/get_index_data.js
@@ -1,15 +1,15 @@
-import axios from "axios";
+// import axios from "axios";
 
-export default async function handler(req, res) {
-  try {
-    const data = await axios.get(
-      "https://meme-api.herokuapp.com/gimme/",
-      { param: req.body.param },
-      headers
-    );
-    res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    return res.status(error.status || 500).end(error.message);
-  }
-}
+// export default async function handler(req, res) {
+//   try {
+//     const data = await axios.get(
+//       "https://meme-api.herokuapp.com/",
+//       { param: req.body.param },
+//       headers
+//     );
+//     res.status(200).json(data);
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(error.status || 500).end(error.message);
+//   }
+// }
